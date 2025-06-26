589bf20f830ee961686c4a759364d3553c1f8410
VictorDTesla24 <melbvicduque@gmail.com>
2025-06-26 13:16:50 +1000

    fix(client): remove duplicated backend source code from client directory

--- client/src/core/analysis/divisional/DashamsaAnalyzer.js
+++
@@ -1,9 +0,0 @@
-// Dashamsa (D10) chart analysis for career
-function analyzeDashamsaCareer(dashamsaChart) {
-  // Example: Look at 10th house and its lord
-  const tenthHouse = dashamsaChart.houses[10];
-  const tenthLord = dashamsaChart.houseLords[10];
-  return { tenthHouse, tenthLord };
-}
-
-module.exports = { analyzeDashamsaCareer };
--- client/src/core/analysis/divisional/DwadasamsaAnalyzer.js
+++
@@ -1,9 +0,0 @@
-// Dwadasamsa (D12) chart analysis for parents
-function analyzeDwadasamsaParents(dwadasamsaChart) {
-  // Example: Look at 4th house (mother), 9th house (father)
-  const motherHouse = dwadasamsaChart.houses[4];
-  const fatherHouse = dwadasamsaChart.houses[9];
-  return { motherHouse, fatherHouse };
-}
-
-module.exports = { analyzeDwadasamsaParents };
