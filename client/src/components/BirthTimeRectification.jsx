c35ec33ec1d8ef4f11ef7c9570e190fb73823b4f
VicsGitRepos <melbvicduque@gmail.com>
2025-11-03 01:03:10 +1100

    Commiting after UI/UX & UI/UX --> Backend Integration/API Endpoints Issues fixed PASS 6 - 03 NOV 2025 01:05 AM Vik

--- client/src/components/BirthTimeRectification.jsx
+++ client/src/components/BirthTimeRectification.jsx
@@ -5,7 +5,7 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { getApiUrl } from '../utils/apiConfig';
+import { getApiUrl } from '../utils/apiConfig.js';
 
 const BirthTimeRectification = ({ 
   initialBirthData = {}, 
